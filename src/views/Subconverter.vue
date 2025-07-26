8eba386a3084b757c5d717dae52bc2c183fe960e
00000OO <wvvvvvwwwww@gmail.com>
2025-07-26 18:39:04 +0800

    Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -188,9 +188,7 @@
                 </el-button>
               </el-form-item>
               <el-form-item label-width="0px" style="text-align: center">
-                <el-button style="width: 250px;" type="success" icon="el-icon-video-play"
-                  @click="centerDialogVisible = true">视频教程
-                </el-button>
+
               </el-form-item>
 
 
@@ -202,23 +200,6 @@
 
 
 
-    <el-dialog title="请选择需要观看的视频教程" :visible.sync="centerDialogVisible" :show-close="false" width="40vh" top="30vh"
-      center>
-      <div label-width="0px" style="text-align: center">
-        <el-button style="width: 200px;" type="primary" icon="el-icon-video-play"
-          @click="gotoBasicVideo(); centerDialogVisible = false">基础视频教程
-        </el-button>
-      </div>
-      <div label-width="0px" style="text-align: center;margin: 3vh 0 2vh">
-        <el-button style="width: 200px;" type="danger" icon="el-icon-video-play"
-          @click="gotoAdvancedVideo(); centerDialogVisible = false">进阶视频教程
-        </el-button>
-      </div>
-      <div label-width="0px" style="text-align: center;margin: 3vh 0 2vh">
-        <el-button style="width: 200px;" type="warning" icon="el-icon-download" @click="toolsDown">代理工具集合
-        </el-button>
-      </div>
-    </el-dialog>
     <el-dialog :visible.sync="dialogUploadConfigVisible" :show-close="false" :close-on-click-modal="false"
       :close-on-press-escape="false" width="80%">
       <el-tabs v-model="activeName" type="card">
